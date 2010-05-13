5fb3ca354d9077bdd1872b6c18373c9228632007
dlukyanov@ukr.net <dlukyanov@ukr.net@977813f8-cfc5-11de-9aee-2b7272db14c4>
2010-05-13 19:45:58 +0000

    new work-copy scan (not finished)

--- trunk/src/easystr.cpp
+++ trunk/src/easystr.cpp
@@ -21,12 +21,12 @@
 
 //removes right spaces
 char*rtrim(char*c){
-	char*ns=c;
 	char*space=" \t\r\n";
-	for(int i=0;c[i];i++)
-		if(!strchr(space,c[i]))
-			ns=c+i+1;
-	ns[0]=0;
+	int len=strlen(c);
+	for(int i=len-1;i>=0;i--){
+		if(strchr(space,c[i]))c[i]=0;
+		else break;
+	}
 	return c;
 }
 
